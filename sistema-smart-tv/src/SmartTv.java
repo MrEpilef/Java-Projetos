e4ba16d06ad25fe1a06156f343feee778967c160
Felipe Miguel <felipemiguelpma@gmail.com>
2025-03-11 23:10:28 -0300

    feat: Nova feature
    Agora o programa perguntara para qual programa você quer que ele deixe

--- sistema-smart-tv/src/SmartTv.java
+++ sistema-smart-tv/src/SmartTv.java
@@ -3,7 +3,7 @@ public class SmartTv {
     boolean ligada = false;  //variável que irá guardar o status da smarTv, sendo "Ligada=True" ou "Desligada=False"
     int canal=1; //Variavel que armazena em qual canal a smarTv está no momento!
     int volume=25; // váriavel que armazena o volume da smarTv
-
+    static int novoCanal=(int)0;
 
     public void trocarCanal(int novoCanal){
         /** Método para indicar diretamente para qual canal  a SmartTv irá
--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -1,3 +1,6 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 /**<H1>Programa SmartTv</H1>
 *   O programa SmartTv permite operar uma tv por comandos, com operações simples de Ligar/Desligar,
 *   aumentar ou diminuir o volumel e canal, ou trocar de canal diretamente.
@@ -14,7 +17,7 @@ public class Usuario {
 
 
     public static void main(String [] args) throws Exception {
-
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
         SmartTv smartTv = new SmartTv();
 
         System.out.println("Tv ligada? " + smartTv.ligada);
@@ -27,8 +30,11 @@ public class Usuario {
         smartTv.aumentarVolume();
         System.out.println("Aumentei o volume para: " + smartTv.volume);
 
-        smartTv.trocarCanal(15);
-        System.out.println("Troquei o canal para o canal: " + smartTv.canal);
+        System.out.println("Qual canal o(a) senhor(a) escolhe?");
+        SmartTv.novoCanal = scanner.nextInt();
+        smartTv.trocarCanal(SmartTv.novoCanal);
+
+        System.out.println("Troquei o canal da tv para o: " + smartTv.canal);
         smartTv.aumentarCanal();
         smartTv.aumentarCanal();
         System.out.println("Coloquei no canal "+ smartTv.canal + " agora");
@@ -37,7 +43,7 @@ public class Usuario {
         System.out.println("Diminui o volume para: " + smartTv.volume);
 
         smartTv.desligar();
-        System.out.println("ACho que vou dormir...");
+        System.out.println("Acho que vou dormir...");
         System.out.println(smartTv.ligada);
 
     }
