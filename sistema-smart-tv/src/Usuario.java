d6428b78691f09ee8adcac3030c6d273a80a001e
Felipe Miguel <felipemiguelpma@gmail.com>
2025-03-11 21:07:08 -0300

    feat: Projeto SmartTv
    Projeto smartTv, com funcionalidade de aumentar/dimuir canais, volumes, e Ligar/Desligar a tv.

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,29 @@
+public class Usuario {
+
+
+    public static void main(String [] args) throws Exception {
+
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("Tv ligada? " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo status -> Tv ligada? " + smartTv.ligada);
+
+        smartTv.aumentarVolume();
+        System.out.println("Aumentei o volume para: " + smartTv.volume);
+
+        smartTv.trocarCanal(15);
+        System.out.println("Troquei o canal para o canal: " + smartTv.canal);
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+        System.out.println("Coloquei no canal "+ smartTv.canal + " agora");
+
+        smartTv.diminuirVolume();
+        System.out.println("Diminui o volume para: " + smartTv.volume);
+
+    }
+
+}
