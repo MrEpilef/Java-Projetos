b6e3bf6ac262ce03e107b9fbd524b84c94b0e74d
Felipe Miguel <felipemiguelpma@gmail.com>
2025-04-18 12:18:05 -0300

    feat: Atividade de Getters and Setters com modificadores de acesso

---
+++ Escola/src/escola/Escola.java
@@ -0,0 +1,11 @@
+package src.escola;
+
+public class Escola {
+    public static void main(String[] args) {
+        Aluno felipe = new Aluno();
+        felipe.setNome("Felipe");
+        felipe.setIdade(10);
+        System.out.println("O aluno " + felipe.getNome() + " tem " + felipe.getIdade() + " anos de idade");
+
+    }
+}
